ea9d162effda1387fb7ca8fb48397564e73d5761
Tsuyoshi Numano <numano.tsuyoshi@lancers.co.jp>
2018-09-12 02:42:50 +0900

    feat: add container and show redux state

--- 03_create_todo_with_redux/react-redux-todo-tutorial/src/container/TodoContainer.js
+++ 03_create_todo_with_redux/react-redux-todo-tutorial/src/container/TodoContainer.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {
+        todo: state.todo,
+    };
+}
 
 class TodoContainer extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: '',
-      todoList: [],
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.addList = this.addList.bind(this);
-    this.doneTodo = this.doneTodo.bind(this);
-  }
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-  addList() {
-    this.setState(prevState => ({
-      todoList: [...prevState.todoList, {
-        value: this.state.value,
-        doneFlag: false,
-      }]
-    }));
-    this.setState({ value: "" });
-  }
-  doneTodo(number) {
-    const newTodoList = this.state.todoList.map((todo, index) => {
-      if(index === number){
-        todo.doneFlag = true;
-      }
-      return todo;
-    })
-    this.setState({ todoList: newTodoList })
-  }
-
   render() {
+    const { data } = this.props.todo;
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.addList}>add</button>
         <div>
-          {this.state.todoList.map((todo, index) =>
-            <div onClick={()=>{this.doneTodo(index)}}>
+          {data.map((todo, index) =>
+            <div>
               {todo.doneFlag ? <del>{todo.value}</del> : todo.value}
             </div>)
           }
@@ -54,4 +24,4 @@ class TodoContainer extends Component {
   }
 }
 
-export default TodoContainer;
+export default connect(mapStateToProps, {})(TodoContainer);
