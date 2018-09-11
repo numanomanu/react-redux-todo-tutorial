bbe849409a8333e26469f19ba78b04e2663e1034
Tsuyoshi Numano <numano.tsuyoshi@lancers.co.jp>
2018-09-12 03:28:59 +0900

    feat: add todo button action

--- 03_create_todo_with_redux/react-redux-todo-tutorial/src/container/TodoContainer.js
+++ 03_create_todo_with_redux/react-redux-todo-tutorial/src/container/TodoContainer.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import * as toDoActions from '../actions/todo';
+
 function mapStateToProps(state) {
-    return {
-        todo: state.todo,
-    };
+  return {
+    todo: state.todo,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    toDoActions: bindActionCreators(toDoActions, dispatch),
+  };
 }
 
 class TodoContainer extends Component {
+
+  addTodo = () => {
+    const dummydata = {
+      value: 'new todo' + this.props.todo.data.length,
+      doneFlag: false,
+    }
+    this.props.toDoActions.addTodo(dummydata);
+  }
   render() {
     const { data } = this.props.todo;
     return (
       <div>
+        <button onClick={this.addTodo}> addTodo </button>
         <div>
           {data.map((todo, index) =>
             <div>
@@ -24,4 +42,4 @@ class TodoContainer extends Component {
   }
 }
 
-export default connect(mapStateToProps, {})(TodoContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoContainer);
